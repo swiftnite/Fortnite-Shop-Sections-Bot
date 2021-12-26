bed552bde498a072615d6d17c7f0aeb5182795d4
Swift-Nite <74127135+swiftnite@users.noreply.github.com>
2021-12-27 08:52:41 +1100

    Update config.py

--- config.py
+++ config.py
@@ -18,10 +18,9 @@ class customisation:
     Heading: str = "Today's Shop Sections:" #NOTE: This will be the text displayed in the post above the sections
     Brackets: str = "True" #NOTE: THIS IS EITHER True or False! True means that the quantity will have brackets around it -> (X7) false means it won't -> X7
     point: str = "-" #NOTE: This is the thing that will appear before the section name, For example it would currently look like -Marvel (X7), you can change the - before the section name if you wish. NOTE: not everything works!!
-    image: str = "True" #NOTE: THIS IS EITHER True or False! This decides whether or not you would like the sections to post with an image of the sections. EXAMPLE: https://cdn.discordapp.com/attachments/752738820306239608/868765701152833566/image0.jpg
 
 '''
 If you have any issues please message me on discord or twitter and I will respond as quick as possible!!
 Twitter: @SwiftNite
-Discord: Swift-nite#3722
+Discord: Swift-nite#1313
 '''
