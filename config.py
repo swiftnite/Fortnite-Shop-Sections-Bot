fe71236a1fa01b5e10b215770fa9e46ace93599a
Swift-Nite <74127135+swiftnite@users.noreply.github.com>
2021-12-27 09:16:15 +1100

    Update config.py

--- config.py
+++ config.py
@@ -16,6 +16,7 @@ The below section is for the customisation of the tweet itself
 '''
 class customisation:
     Heading: str = "Today's Shop Sections:" #NOTE: This will be the text displayed in the post above the sections
+    Language: str = "en" #NOTE: This is the language that the sections are displayed in choose from ar / de / en / es / es-419 / fr / it / ja / ko / pl / pt-BR / ru / tr
     Brackets: str = "True" #NOTE: THIS IS EITHER True or False! True means that the quantity will have brackets around it -> (X7) false means it won't -> X7
     point: str = "-" #NOTE: This is the thing that will appear before the section name, For example it would currently look like -Marvel (X7), you can change the - before the section name if you wish. NOTE: not everything works!!
 
