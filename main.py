2e4d262f8988510213e7e493262c60c5d3d50d32
Swift-Nite <74127135+swiftnite@users.noreply.github.com>
2022-02-19 14:28:07 +1100

    Lang and error fixes
    Fixed lang support (hopefully)
    Fixed a few slight errors
    Bot also now prints an error if you have not entered your API keys into config file

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ import tweepy
 import os, random
 from config import keys, customisation
 consumer_key = keys.consumer_key
-image='False'
 consumer_secret_key = keys.consumer_secret_key
 access_token = keys.access_token
 access_token_secret = keys.access_token_secret
+if not all((consumer_key, consumer_secret_key, access_token,access_token_secret)):
+    print('WARNING!!!\nYou have not entered your Twitter Api keys into the config.json file!\nThis bot will be unable to post unless you enter these keys.\n\n')
 Heading = customisation.Heading
 Brackets = customisation.Brackets
 Language = customisation.Language
@@ -17,15 +18,18 @@ point = customisation.point
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
+with open('translations.json', 'r', encoding='utf8') as translator:
+    translator = json.load(translator)
+sFix1=["20", "19", "18", "17", "16", "15", "14", "13", "12", "11", "10", "9B", "8B", "7B", "6B", "5B", "4B", "3B", "2B", "1B", "9C", "8C", "7C", "6C", "5C", "4C", "3C", "2C", "1C"]
+sFix2=["9", "8", "7", "6", "5", "4", "3", "2", "1", "B", "C"]
 
 def main():
     try:
         url=get('https://api.nitestats.com/v1/epic/modes-smart').json()
         url2=get(f'https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game/shop-sections?lang={Language}').json()['sectionList']['sections']
-
+        
         try:
             sections1=url['channels']['client-events']['states'][1]['state']['sectionStoreEnds']
-
             x = []
             txt = open(f"sections.txt","w+")
             auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
@@ -37,33 +41,44 @@ def main():
 
             if sections1 != cache1:
                 for a in sections1:
+
                     goatt=0
                     for b in url2:
                         name=a
                         sectionId=b['sectionId']
-                        sFix1=["20", "19", "18", "17", "16", "15", "14", "13", "12", "11", "10", "9B", "8B", "7B", "6B", "5B", "4B", "3B", "2B", "1B", "9C", "8C", "7C", "6C", "5C", "4C", "3C", "2C", "1C"]
-                        sFix2=["9", "8", "7", "6", "5", "4", "3", "2", "1", "B", "C"]
                         if name==sectionId:
                             goatt+=1
                             try:
                                 name=b['sectionDisplayName']
                             except:
                                 name=a
-                                if name.endswith(tuple(sFix1)):
-                                    name=name[:-2]
-                                elif name.endswith(tuple(sFix2)):
-                                    name=name[:-1]
-                                if 'Special' in name:
-                                    name='More Offers'
+                                for o in translator:
+                                    if o in name:
+                                        name=translator[o][Language]
+                                        success=True
+                                    else:
+                                        success=False
+                                if success==False:
+                                    if name.endswith(tuple(sFix1)):
+                                        name=name[:-2]
+                                    elif name.endswith(tuple(sFix2)):
+                                        name=name[:-1]
                             x.append(name)
                         else:
                             pass
                     if goatt==0:
                         name=a
-                        if name.endswith(tuple(sFix1)):
-                            name=name[:-2]
-                        elif name.endswith(tuple(sFix2)):
-                            name=name[:-1]
+                        for o in translator:
+                            if o in name:
+                                name=translator[o][Language]
+                                success=True
+                            else:
+                                success=False
+                        if success==False:
+                            if name.endswith(tuple(sFix1)):
+                                name=name[:-2]
+                            elif name.endswith(tuple(sFix2)):
+                                name=name[:-1]
                         x.append(name)
                 x=sorted(x)
                 count=Counter(x)
