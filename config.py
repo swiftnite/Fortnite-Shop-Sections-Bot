c653eb950f6a3825f9fdff748f0f4b1191215db3
Swift-Nite <74127135+swiftnite@users.noreply.github.com>
2022-02-20 17:38:38 +1100

    Way more customisation options!! (config file)
    New customisation options include:
    showIfOne (which shows number of sections even if only one)
    quantitySymbol (which allows you to set the symbol or string before or after number of sections could be 7 tabs or x7 or X7)
    beforeOrAfter (Allows you to set the quantitySymbol to appear before or after the number of sections)
    Footer (this will appear under the sections, this is optional!)

--- config.py
+++ config.py
@@ -16,10 +16,14 @@ The below section is for the customisation of the tweet itself
 '''
 class customisation:
     Heading: str = "Today's Shop Sections:" #NOTE: This will be the text displayed in the post above the sections
+    Footer: str = "" #NOTE: This is what will appear under the sections! Leave blank("") if you do not want a footer
     Language: str = "en" #NOTE: This is the language that the sections are displayed in choose from ar / de / en / es / es-419 / fr / it / ja / ko / pl / pt-BR / ru / tr
-    Brackets: str = "True" #NOTE: THIS IS EITHER True or False! True means that the quantity will have brackets around it -> (X7) false means it won't -> X7
-    point: str = "-" #NOTE: This is the thing that will appear before the section name, For example it would currently look like -Marvel (X7), you can change the - before the section name if you wish. NOTE: not everything works!!
+    point: str = "- " #NOTE: This is the thing that will appear before the section name, For example it would currently look like - Marvel (X7), you can change the "-" before the section name if you wish. NOTE: not everything works!!
 
+    Brackets: bool = True #NOTE: THIS IS EITHER True or False! True means that the quantity will have brackets around it -> (X7) false means it won't -> X7
+    showIfOne: bool = True #NOTE: THIS IS EITHER True or False! True means that the if the quantity of a section is one it will display 1 with your quantitySymbol. False means that the quantity will be hidden if 1!
+    quantitySymbol: str = "x"  #NOTE: This is what will appear either before or after the quantity of that section. For example X7 where this is "X". Another example is 7 tabs where this is " tabs"
+    beforeOrAfter: str = "Before" #NOTE: THIS IS EITHER "Before" or "After"! Before means that the quantitySymbol will be before the quantity number of the sections
 '''
 If you have any issues please message me on discord or twitter and I will respond as quick as possible!!
 Twitter: @SwiftNite
